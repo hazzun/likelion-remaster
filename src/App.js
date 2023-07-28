c2c23d6a19c83fe8ee1dc5cc0dc0b5ca020ad08f
hajoon <gkwns0516@gmail.com>
2023-07-28 15:57:58 +0900

    [Init] 웹 모바일 뷰 초기세팅 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 function App() {
   return (
-    <div className='bg-white h-screen'>
-      <header className='text-sm'>멋사 해커톤 가능?</header>
+    <div className='bg-white min-h-screen'>
+      <header></header>
+      <main>
+        <div className='text-sm text-center'>멋사 해커톤 가능?</div>
+      </main>
+      <footer></footer>
     </div>
   );
 }

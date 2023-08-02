4471b193c7792806f5a500baffa4ebe3059053b1
jaqwe2301 <jaqwe2301@naver.com>
2023-08-03 04:16:05 +0900

    Feat: header 스타일링

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import { ReactComponent as BackButton } from "../assets/svg/back-button.svg";
 
 export default function Header() {
   return (
-    <div className='max-w-[480px] w-full fixed top-0 h-[56px] bg-gray-700 text-white p-4 z-20'>
-      Header
+    <div className="flex justify-between items-center px-5 max-w-[480px] w-full fixed top-0 h-[56px] bg-[#F9F9F9]">
+      <BackButton />
+      <p className="text-black font-['Pretendard'] text-[1.25rem]">에이블</p>
+      <div className="w-[9px]" />
     </div>
   );
 }

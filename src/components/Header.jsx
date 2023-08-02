62c17c866b98262efe1a63526de9e3d40f0b7c5b
jaqwe2301 <jaqwe2301@naver.com>
2023-08-03 08:15:52 +0900

    Feat: 사용자 선택, 도움 글 작성 page1 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { ReactComponent as BackButton } from "../assets/svg/back-button.svg";
 
-export default function Header() {
+export default function Header({ title }) {
+  const navigate = useNavigate();
+
   return (
     <div className="flex justify-between items-center px-5 max-w-[480px] w-full fixed top-0 h-[56px] bg-[#F9F9F9]">
-      <BackButton />
-      <p className="text-black font-['Pretendard'] text-[1.25rem]">에이블</p>
+      <button onClick={() => navigate(-1)}>
+        <BackButton />
+      </button>
+      <p className="text-[1.25rem]">{title}</p>
       <div className="w-[9px]" />
     </div>
   );

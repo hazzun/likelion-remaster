b90fbe65ecc83d2dbb6055aafe3f65ea8cb5ece5
hajoon <gkwns0516@gmail.com>
2023-08-14 17:27:46 +0900

    [Refactor] modal 컴포넌트 디렉토리로 분류

--- src/components/recipient/Record.jsx
+++ src/components/recipient/Record.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { AudioRecorder, useAudioRecorder } from 'react-audio-voice-recorder';
-import SaveModal from '../SaveModal';
+import SaveModal from '../modal/SaveModal';
 import { BsFillMicFill, BsFillStopFill } from 'react-icons/bs';
 import axios from 'axios';
 
 export default function Record() {
-
-  // 추후 로그인 검사코드 추가 
+  // 추후 로그인 검사코드 추가
   const [usertoken, setUsertoken] = useState('');
 
   useEffect(() => {
@@ -17,107 +16,112 @@ export default function Record() {
     {
       noiseSuppression: true,
       echoCancellation: true,
-    }, (err) => console.table(err)
+    },
+    (err) => console.table(err)
   );
-  
+
   /* 녹음 후 생성되는 audio 요소 관리 */
   const [existAudio, setExistAudio] = useState(false);
   const [t_blob, setT_blob] = useState();
   const addAudioElement = (blob) => {
-    const saveBtn = document.getElementById('saveBtn')
-    const url = URL.createObjectURL(blob)
-    setT_blob(blob)
-    if(existAudio) {
-      const audio = document.querySelector('audio')
-      audio.src = url
-      audio.controls = true
-      document.getElementById('myDiv').insertBefore(audio, saveBtn)
+    const saveBtn = document.getElementById('saveBtn');
+    const url = URL.createObjectURL(blob);
+    setT_blob(blob);
+    if (existAudio) {
+      const audio = document.querySelector('audio');
+      audio.src = url;
+      audio.controls = true;
+      document.getElementById('myDiv').insertBefore(audio, saveBtn);
     } else {
-      const audio = document.createElement('audio')
-      setExistAudio(true)
-      audio.src = url
-      audio.controls = true
-      audio.className += "hidden"
-      document.getElementById('myDiv').insertBefore(audio, saveBtn)
+      const audio = document.createElement('audio');
+      setExistAudio(true);
+      audio.src = url;
+      audio.controls = true;
+      audio.className += 'hidden';
+      document.getElementById('myDiv').insertBefore(audio, saveBtn);
     }
   };
 
   /* 녹음 시작 멈춤 관리 */
   const [recording, setRecording] = useState(false);
   useEffect(() => {
-    if(!recording) {  
+    if (!recording) {
       recorderControls.stopRecording();
-      console.log("녹음 중지")
-    }
-    else{
+      console.log('녹음 중지');
+    } else {
       recorderControls.startRecording();
-      console.log("녹음 시작")
+      console.log('녹음 시작');
     }
   }, [recording]);
-  
+
   /* 재생 시작 멈춤 관리 */
   const [start, setStart] = useState(false);
   const clickListenBtn = () => {
     const audio = document.querySelector('audio');
-    if(audio != null){
-      if(start) {
+    if (audio != null) {
+      if (start) {
         setStart(false);
         audio.pause();
-        audio.currentTime = 0
-        console.log("재생 멈춤")
-      }
-      else {
+        audio.currentTime = 0;
+        console.log('재생 멈춤');
+      } else {
         setStart(true);
         audio.play();
-        console.log("재생 시작")
+        console.log('재생 시작');
       }
     } else {
-      alert("녹음된 내용이 없습니다!")
+      alert('녹음된 내용이 없습니다!');
     }
-  }
-  
-  const [modalShow, setModalShow] = useState(false)
+  };
+
+  const [modalShow, setModalShow] = useState(false);
   const clickHelp = () => {
-    if(existAudio) {
-      setModalShow(true)
+    if (existAudio) {
+      setModalShow(true);
+    } else {
+      alert('녹음 후 도움을 요청해 주세요 :)');
     }
-    else {
-      alert("녹음 후 도움을 요청해 주세요 :)")
-    }
-  }
+  };
 
   return (
     <div className='w-full h-full flex flex-col justify-between pb-[2.125rem] bg-white px-5'>
       <div className='font-semibold text-[24px] py-5'>
-          도움이 필요한 내용을 녹음해 주세요.
+        도움이 필요한 내용을 녹음해 주세요.
       </div>
-      <div className="hidden">
+      <div className='hidden'>
         <AudioRecorder
           onRecordingComplete={(blob) => addAudioElement(blob)}
           recorderControls={recorderControls}
           showVisualizer={true}
-          className="hidden"
+          className='hidden'
         />
       </div>
-      <div className="flex gap-5 items-center justify-center">
-        <button onClick={(e) => setRecording(!recording)}
-                  className='bg-[#5C5C5C] rounded-full p-5'
+      <div className='flex gap-5 items-center justify-center'>
+        <button
+          onClick={(e) => setRecording(!recording)}
+          className='bg-[#5C5C5C] rounded-full p-5'
         >
-          { !recording ? 
-              <BsFillMicFill color='#FFC700' size='50'/> :
-              <BsFillStopFill color='#FFC700' size='50' className='blinking'/>
-          }
+          {!recording ? (
+            <BsFillMicFill color='#FFC700' size='50' />
+          ) : (
+            <BsFillStopFill color='#FFC700' size='50' className='blinking' />
+          )}
         </button>
       </div>
-      <div id='myDiv' className='flex flex-col items-center justify-center gap-5'>
-        <button className='w-full h-[3.25rem] text-xl font-medium rounded-2xl bg-[#5C5C5C] text-[#FFC700]' 
-                onClick={() => clickListenBtn()}
+      <div
+        id='myDiv'
+        className='flex flex-col items-center justify-center gap-5'
+      >
+        <button
+          className='w-full h-[3.25rem] text-xl font-medium rounded-2xl bg-[#5C5C5C] text-[#FFC700]'
+          onClick={() => clickListenBtn()}
         >
           녹음된 내용 확인하기
         </button>
-        <button id='saveBtn' 
-                className='w-full h-[3.25rem] text-xl font-medium rounded-2xl bg-[#FFC700]' 
-                onClick={() => clickHelp()}
+        <button
+          id='saveBtn'
+          className='w-full h-[3.25rem] text-xl font-medium rounded-2xl bg-[#FFC700]'
+          onClick={() => clickHelp()}
         >
           도움 요청하기
         </button>
@@ -125,8 +129,9 @@ export default function Record() {
 
       <SaveModal
         isVisible={modalShow}
-        onClose={() => setModalShow(false)} 
-        usertoken={usertoken} fileBlob={t_blob}
+        onClose={() => setModalShow(false)}
+        usertoken={usertoken}
+        fileBlob={t_blob}
       />
     </div>
   );
